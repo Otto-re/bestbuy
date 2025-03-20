aee3bb69d31e9c5404609a83ad1e60795c8e6c82
Otto-re <otto-reifschneider@t-online.de>
2025-03-20 16:16:46 +0100

    fill the store and implement it to the main.py

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
-from products import Product
-
-def main():
-    bose = Product("Bose QuietComfort Earbuds", price=250, quantity=500)
-    mac = Product("MacBook Air M2", price=1450, quantity=100)
-
-    print(bose.buy(50))  # 50 * 250 = 12500
-    print(mac.buy(100))  # 100 * 1450 = 145000
-    print(mac.is_active())  # False, weil Menge 0 ist
-
-    print(bose.show())  # "Bose QuietComfort Earbuds, Preis: 250, Menge: 450"
-    print(mac.show())   # "MacBook Air M2, Preis: 1450, Menge: 0"
-
-    bose.set_quantity(1000)
-    print(bose.show())  # "Bose QuietComfort Earbuds, Preis: 250, Menge: 1000"
+import products
+from store import Store
 
 if __name__ == "__main__":
-    main()
+
+    bose = products.Product("Bose QuietComfort Earbuds", price=250, quantity=500)
+    mac = products.Product("MacBook Air M2", price=1450, quantity=100)
+    pixel = products.Product("Google Pixel 7", price=500, quantity=250)
+
+    best_buy = Store([bose, mac])
+    best_buy.add_product(pixel)
+
+    price = best_buy.order([(bose, 5), (mac, 5), (bose, 5),])
+    print(f"order cost: {price} dollers.")
+
+    print(best_buy.get_total_quantity())
+    for product in best_buy.get_all_products():
+        print(product.show())
+
